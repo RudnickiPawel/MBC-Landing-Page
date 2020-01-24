1f91cbc3f2b2c65d87259f43230401f9dd055d56
RudnickiPawel <rudnicki.pawel94@gmail.com>
2020-01-24 17:18:23 +0100

    app-preview finished, but code needs cleaning

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  //swap images when hovered
   $('.footer__img-hover-style').hover(function () {
     var img = $(this).find('img')[0];
     if (img != undefined) {
@@ -23,4 +25,24 @@ $(document).ready(function () {
       return attr.replace("_hovered.png", ".png");
     });
   });
+  // app-preview opening default tab
+  document.getElementById("app-preview__link1").click();
+
 });
+
+  // app-preview toggable tabs
+  function displayTabItem(event, content_class) {
+    var i, tabcontent, tablinks, content_to_display;
+    tabcontent = $('.app-preview__item');
+    for (i = 0; i < tabcontent.length; i++)
+      tabcontent[i].style.display = "none";
+
+    tablinks = $('.app-preview__links');
+    for (i = 0; i < tablinks.length; i++)
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+    content_to_display = $('.'+content_class);
+    for(i=0;i<content_to_display.length;i++)
+    content_to_display[i].style.display = "flex";
+    event.currentTarget.className += " active";
+  }

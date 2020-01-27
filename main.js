a6e369dfbc489d8133d937f17af4729933c4b8cf
RudnickiPawel <rudnicki.pawel94@gmail.com>
2020-01-27 10:51:48 +0100

    styling currently active link in app-preview

--- main.js
+++ main.js
@@ -27,22 +27,31 @@ $(document).ready(function () {
   });
   // app-preview opening default tab
   document.getElementById("app-preview__link1").click();
-
+  //arranging active link
+  $('.app-preview__links').click(function () {
+    removeActiveFromAll();
+    $(this).addClass("app-preview__active-link");
+  });
 });
 
-  // app-preview toggable tabs
-  function displayTabItem(event, content_class) {
-    var i, tabcontent, tablinks, content_to_display;
-    tabcontent = $('.app-preview__item');
-    for (i = 0; i < tabcontent.length; i++)
-      tabcontent[i].style.display = "none";
+// app-preview toggable tabs
+function displayTabItem(event, content_class) {
+  var i, tabcontent, tablinks, content_to_display;
+  tabcontent = $('.app-preview__item');
+  for (i = 0; i < tabcontent.length; i++)
+    tabcontent[i].style.display = "none";
 
-    tablinks = $('.app-preview__links');
-    for (i = 0; i < tablinks.length; i++)
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  tablinks = $('.app-preview__links');
+  for (i = 0; i < tablinks.length; i++)
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
 
-    content_to_display = $('.'+content_class);
-    for(i=0;i<content_to_display.length;i++)
+  content_to_display = $('.' + content_class);
+  for (i = 0; i < content_to_display.length; i++)
     content_to_display[i].style.display = "flex";
-    event.currentTarget.className += " active";
-  }
+  event.currentTarget.className += " active";
+}
+
+//removing active class from links in app-preview
+function removeActiveFromAll() {
+  $('.app-preview__links').removeClass("app-preview__active-link")
+}
